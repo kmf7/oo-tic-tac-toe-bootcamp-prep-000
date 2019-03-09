eda3182533141aa1066566eeddbfd404b52e2087
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-09 17:15:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ class TicTacToe
   def move(index, token = "X")
     @board[index] = token
   end
-  def position_taken?(index)
+  def position_taken?
     @board[index] == "X" || @board[index] == "O"? true : false
   end
   def valid_move?(index)
