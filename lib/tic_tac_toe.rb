a5175be5a78baf0aaf70c35f003ee39669c9678f
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-03-09 17:19:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ class TicTacToe
     @board[index] == "X" || @board[index] == "O"? true : false
   end
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?
+    index <= 8 && index>= 0 && !position_taken?
   end
   def turn 
     puts "Please enter 1-9:"
